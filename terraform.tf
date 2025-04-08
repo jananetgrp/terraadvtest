f1d78ba558ac173b29e1599e729790175c20ac3b
jananetgrp <jananetgrp@gmail.com>
2025-04-08 15:18:04 -0400

    Fix: Updated terraform

--- terraform.tf
+++ terraform.tf
@@ -4,16 +4,7 @@ terraform {
       source = "hashicorp/aws"
     }
   }
-
-  backend "s3" {
-    bucket         = "terraform-backend-terraformbackends3bucket-guadwb7hv7jm"
-    key            = "pipeline"
-    region         = "us-east-1"
-    use_lockfile = true
-    profile = "singj6_aws1-prodtest"
-  }
 }
-
 provider "aws" {
   default_tags {
     tags = {
